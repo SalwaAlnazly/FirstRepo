0e4f5495d4b73bb24336f57e3c8c1e91aeabad6e
salwaAlnazly <herotony111@gmail.com>
2019-01-03 07:56:25 +0200

    creating radio btn section

--- dominralo/src/App.js
+++ dominralo/src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+import './App.css'
+import RadioBtnSection from './container/RadioBtnSection'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={RadioBtnSection} />
+            <Route exact path='*' render={() => <h1>Page not found</h1>} />
+          </Switch>
+        </Router>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
+
+
