b8f0c39e4724e039f132826eceaaafe37444cc17
salwaAlnazly <herotony111@gmail.com>
2019-01-23 22:30:43 +0200

    receive event in server side

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
+io.sockets.on('connection', (socket) => {
+    socket.on("send message", (newMsg) => {
+        io.sockets.emit("new message", newMsg)
+    })
+}) 
+
 http.listen(3000, () => {
     console.log('Server is running on port :3000')
 })
