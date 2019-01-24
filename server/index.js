96834713ed6b82ec9e9899bc7357132bf89efa21
salwaAlnazly <herotony111@gmail.com>
2019-01-24 21:04:55 +0200

    creating server folder and set up the main server with socket.io

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const app = require('express')()
+const server = require('http').Server
+const io = require('socket.io')(server)
+const ChatSocket = require('./chat-socket')
+
+
+io.on('connection', ChatSocket);
+
+app.listen('8080', () => {
+    console.log('server is running on port: ', port)
+})
