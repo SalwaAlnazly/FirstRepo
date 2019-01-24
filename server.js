7ca3a0523caca3092c8faa171b88ec9655ef1b62
salwaAlnazly <herotony111@gmail.com>
2019-01-24 10:07:04 +0200

    make some changes

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ const app = require('express')();
 const server = require('http').Server(app)
 const io = require('socket.io')(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get('/' , (req,res)=>{
+    res.sendFile(__dirname+'/index.html');
 })
 
-io.sockets.on('connection', (socket) => {
-    socket.on("send message", (newMsg) => {
-        io.sockets.emit("new message", newMsg)
-        // socket.broadcast.emit("new message", newMsg)
+
+io.on('connection', (socket) => { 
+    socket.on('send message', newMsg => {
+        socket.emit("new message", newMsg)
+        socket.broadcast.emit("new message", newMsg)
     })
 }) 
 
-http.listen(3000, () => {
+server.listen(3000, () => {
     console.log('Server is running on port :3000')
 })
