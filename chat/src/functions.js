1a96f4b25450c1e52b99601c035fee1b716c55af
salwaAlnazly <herotony111@gmail.com>
2019-01-25 10:46:49 +0200

    create a user

--- chat/src/functions.js
+++ chat/src/functions.js
@@ -1,1 +1,12 @@
+const uuidv4 = require('uuid/v4');
+
 /** create a user  */
+const createUser = ({name = ""} = {}) => ({
+       id: uuidv4,
+       name
+})
+
+
+module.exports = {
+    createUser
+}
