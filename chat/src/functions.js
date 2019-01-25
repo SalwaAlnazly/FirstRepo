6f5c6fd41e40a17554ed315792ca090e845aea49
salwaAlnazly <herotony111@gmail.com>
2019-01-25 10:54:53 +0200

    create a  messages object

--- chat/src/functions.js
+++ chat/src/functions.js
@@ -2,11 +2,18 @@ const uuidv4 = require('uuid/v4');
 
 /** create a user  */
 const createUser = ({name = ""} = {}) => ({
-       id: uuidv4,
+       id: uuidv4(),
        name
 })
 
+const createMessage = ({message = "", sender= ""} = {}) => ({
+    id: uuidv4(),
+    time: new Date(Date.now()),
+    message,
+    sender
+})
 
 module.exports = {
-    createUser
+    createUser,
+    createMessage
 }
