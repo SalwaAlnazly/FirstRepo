6b2f3827e4d342be3831d5da47979669971cb894
salwaAlnazly <herotony111@gmail.com>
2019-01-26 21:36:07 +0200

    check the submitted property inside state and render the chat room if this true

--- chat/src/components/loginForm.js
+++ chat/src/components/loginForm.js
@@ -1,21 +1,27 @@
 import React from 'react'
+import ChatRoom from 'chatRoom'
 
 export default class LoginFormWithNickname extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props)
         this.state = {
-            nickname : "",  // set the initial state of the application
+            nickname: "",  // set the initial state of the application
             submitted: false
         }
     }
     nicknameChangeHandler = (e) => {
-        this.setState({nickname: e.target.value})
+        this.setState({ nickname: e.target.value })
     }
     nicknameSubmitHandler = (e) => {
-          e.preventDefault()
-          this.setState({submitted: true, nickname: this.state.nickname})
+        e.preventDefault()
+        this.setState({ submitted: true, nickname: this.state.nickname })
     }
     render() {
+        if (this.state.submitted) {
+            return (
+                <ChatRoom nickname={this.state.nickname} />
+            )
+        }
         return (
             <form className="nickname-container" onSubmit={this.nicknameSubmitHandler}>
                 <h1>Enter you nickname</h1>
