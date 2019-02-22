c63bd03ad04bdf2c5a5f0849b527288924bfb7f1
salwaAlnazly <herotony111@gmail.com>
2019-02-22 22:25:54 +0200

    connect to banckend io server and listen to socket handlers

--- chat/src/components/socket.js
+++ chat/src/components/socket.js
@@ -1,15 +1,15 @@
 import socketIOClient from "socket.io-client";
 const ioMessagesUrl = 'http://127.0.0.1:8000/messages';
-
 const io = socketIOClient(ioMessagesUrl)
-var start = false
 
-export const init = () => {
-    start = true
-}
+const ioCustomerMessages = 'http://127.0.0.1:8080/messages'
+const ioSocket = socketIOClient(ioCustomerMessages)
 
 export const onOpenConnection = user =>io.emit('adminOpenConnection' , user )
 
+export const fetchUserUrl = userId => ioSocket.emit('fetchUserUrl', userId)
+export const onLoadAdminMessages = cb => ioSocket.on('loadAdminMessages', cb)
+export const onSendMessage = message => ioSocket.emit('message', message)
 
 io.on('error' , error => alert(error.message))
 
