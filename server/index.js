e555a1aee5235b78f34ac6a6ab953dcca9c49b5e
salwaAlnazly <herotony111@gmail.com>
2019-02-22 22:31:23 +0200

    modulize socket io

--- server/index.js
+++ server/index.js
@@ -1,22 +1,26 @@
-const app = require('express')();
+const app = require('express')()
 const http = require('http').Server(app)
-const io = require('socket.io')(http);
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+const io = require('socket.io')(http)
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+
+const messagesSocket = require('./controllers/messages')
+io.of('/messages').on('connection', messagesSocket.bind(this, io))
 
 
-mongoose.connect(require('./config/keys').mongodb.url);
-const db = mongoose.connection;
+
+mongoose.connect(require('./config/keys').mongodb.url)
+const db = mongoose.connection
 
 
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function () {
-  console.log("we're connected to mongoStore");
-});
+  console.log("we're connected to mongo")
+})
 
-//use sessions for tracking logins
+
 app.use(session({
   secret: 'work hard',
   resave: true,
@@ -24,37 +28,29 @@ app.use(session({
   store: new MongoStore({
     mongooseConnection: db
   })
-}));
+}))
 
-// parse incoming requests
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 
-// include routes
-const routes = require('./routes/index');
-app.use('/users', routes);
+const routes = require('./routes/index')
+app.use('/users', routes)
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  const err = new Error('File Not Found');
-  err.status = 404;
-  next(err);
-});
+  const err = new Error('File Not Found')
+  err.status = 404
+  next(err)
+})
 
-// error handler
-// define as the last app.use callback
 app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.send(err.message);
-});
-
-
-http.listen(8080, () => {
-	console.log("Server is running on http://localhost:8080");
+  res.status(err.status || 500)
+  res.send(err.message)
 })
 
 
+http.listen(8080, () => {
+	console.log("Server is running on http://localhost:8080")
+})
 
 
